ec76c4dbe13f4d47c153f5da7e9c003900796075
Kulpot <kulpotwahu@gmail.com>
2023-09-20 07:08:02 +0800

    cmd Program.exe hellllo output

--- CSharp Assemblies/Program.cs
+++ CSharp Assemblies/Program.cs
@@ -4,6 +4,60 @@
 // assembly - .dll, .exe, also creates module
 
 
+/*      ------------CMD---------------------
+ *      
+C:\Users\sunny\source\repos\CSharp Assemblies>cd CSharp Assemblies
+
+C:\Users\sunny\source\repos\CSharp Assemblies\CSharp Assemblies>dir
+ Volume in drive C has no label.
+ Volume Serial Number is DAE4-938D
+
+ Directory of C:\Users\sunny\source\repos\CSharp Assemblies\CSharp Assemblies
+
+20/09/2023  07:00 am    <DIR>          .
+20/09/2023  07:00 am    <DIR>          ..
+20/09/2023  06:54 am               189 App.config
+20/09/2023  06:54 am    <DIR>          bin
+20/09/2023  06:55 am             2,384 CSharp Assemblies.csproj
+20/09/2023  06:54 am    <DIR>          obj
+20/09/2023  07:00 am               272 Program.cs
+20/09/2023  06:54 am    <DIR>          Properties
+               3 File(s)          2,845 bytes
+               5 Dir(s)  490,495,135,744 bytes free
+
+   C:\Users\sunny\source\repos\CSharp Assemblies\CSharp Assemblies>csc Program.cs
+Microsoft (R) Visual C# Compiler version 4.7.0-3.23416.8 (43b0b05c)
+Copyright (C) Microsoft Corporation. All rights reserved.
+
+
+C:\Users\sunny\source\repos\CSharp Assemblies\CSharp Assemblies>dir
+ Volume in drive C has no label.
+ Volume Serial Number is DAE4-938D
+
+ Directory of C:\Users\sunny\source\repos\CSharp Assemblies\CSharp Assemblies
+
+20/09/2023  07:04 am    <DIR>          .
+20/09/2023  07:04 am    <DIR>          ..
+20/09/2023  06:54 am               189 App.config
+20/09/2023  06:54 am    <DIR>          bin
+20/09/2023  06:55 am             2,384 CSharp Assemblies.csproj
+20/09/2023  06:54 am    <DIR>          obj
+20/09/2023  07:00 am               272 Program.cs
+20/09/2023  07:04 am             4,096 Program.exe
+20/09/2023  06:54 am    <DIR>          Properties
+               4 File(s)          6,941 bytes
+               5 Dir(s)  490,494,267,392 bytes free
+
+C:\Users\sunny\source\repos\CSharp Assemblies\CSharp Assemblies>Program.exe
+Helllo
+      
+  
+  
+  
+  
+ * 
+ */
+
 class MainClass
 {
     static void Main()
@@ -11,3 +65,4 @@ class MainClass
         Console.WriteLine("Helllo");
     }
 }
+
