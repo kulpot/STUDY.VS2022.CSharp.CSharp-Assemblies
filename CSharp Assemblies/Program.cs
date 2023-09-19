aee48744816e2538ecaa9c28fe4eb5b880e5bd41
Kulpot <kulpotwahu@gmail.com>
2023-09-20 07:00:53 +0800

    Add project files.

---
+++ CSharp Assemblies/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+//ref link:https://www.youtube.com/watch?v=ky3_3ubDxlI&list=PLRwVmtr-pp06rfSgNYu_oBg40DkwXiRHt&index=2
+// assembly - .dll, .exe, also creates module
+
+
+class MainClass
+{
+    static void Main()
+    {
+        Console.WriteLine("Helllo");
+    }
+}
